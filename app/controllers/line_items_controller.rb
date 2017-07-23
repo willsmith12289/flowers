f79598a7b9e0c43b42fb4a90bad418187a559d36
will <willsmith12289@gmail.com>
2017-07-23 11:52:10 -0400

    working form quantity

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -36,7 +36,8 @@ class LineItemsController < ApplicationController
   # POST /line_items.json
   def create
     product = Product.find(params[:product_id])
-    @line_item = @cart.add_product(product.id)
+    @amount = params[:quantity]
+    @line_item = @cart.add_product(product.id, @amount)
 
     respond_to do |format|
       if @line_item.save
